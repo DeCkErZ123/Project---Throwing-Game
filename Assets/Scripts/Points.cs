71c17f6ccd9ed5865dff5f876c1c8bcaf22b0b06
DeCkErZ123 <131213823+DeCkErZ123@users.noreply.github.com>
2023-06-15 10:22:59 +1000

    Basic Scoring System - Not Working
    Tried implementing a scoring system off of a youtube tutorial. Doesn't work. Gonna try a different one.

---
+++ Assets/Scripts/Points.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Points : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.transform.name == "First Person Player")
+        {
+            KeepScore.Score += 100;
+        }
+    }
+}
